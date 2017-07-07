8f23e81563e96bb2078c531d96f25c7520ee367a
Felipe Sarabia <fsarabia88@gmail.com>
2017-07-07 16:53:52 -0500

    Add files via upload

---
+++ rumkin-hexatbash.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Rumkin HexAtbash
+// @version      0.1
+// @description  letter-pair replacement solver - hex atbash helper
+// @author       Ninjaburger
+// @match        http://rumkin.com/tools/cipher/cryptogram.php?hexatbash
+// @require      http://code.jquery.com/jquery-latest.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(document).ready(function() {
+        var idx = '0123456789ABCDEF';
+        var rev = idx.split('').reverse().join('');
+
+        for(var i = 0; i < idx.length; i++) $('input[name=\'L_' + idx.charAt(i) + '\']').val(rev.charAt(i));
+
+        RecalcAlphabet();
+    });
+})();
